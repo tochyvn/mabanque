300e28a5cd7887d6e2b3494bf3482daccfadc803
tochyvn <tochlionimy@gmail.com>
2017-06-08 10:55:38 +0200

    Maven : ajout de servlet-api et debut de spring-security

--- src/main/java/org/sid/security/SecurityConfig.java
+++ src/main/java/org/sid/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package org.sid.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +10,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("admin").roles("ADMIN", "USER");
+		
+		auth.inMemoryAuthentication()
+		.withUser("user").password("user").roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin();
+		http.authorizeRequests().antMatchers("/operations", "/consulter-compte").hasRole("USER");
+		http.authorizeRequests().antMatchers("/transaction").hasRole("ADMIN");
+	}
+	
+	
 }
